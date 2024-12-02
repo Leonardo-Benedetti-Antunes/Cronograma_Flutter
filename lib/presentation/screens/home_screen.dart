6ff14991292ac6db0701fa03a27d96d4092a6140
leonardo.benedetti <leonardo.benedetti@sbsistemas.com.br>
2024-12-02 17:53:14 -0300

    "Alterações"

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -7,10 +7,33 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cronograma Manager'),
+        title: const Text('Cronograma'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
       ),
       body: const Center(
-        child: Text('Bem-vindo ao Cronograma Manager!'),
+        child: Column(
+          children: [
+            Text('Bem-vindo ao Cronograma!'),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue[400],
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Início',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_customize_rounded),
+            label: 'Cronogramas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_reaction_rounded),
+            label: 'Professores',
+          ),
+        ],
       ),
     );
   }
